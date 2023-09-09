2dd3899dbdec25c835515d08a224c6eb69eea260
ToriMun <132326048+torimun@users.noreply.github.com>
2023-09-09 22:27:15 +0000

    [Add]退会済み会員でのログイン試行時、welcomeページに推移するよう設定

--- app/controllers/customers/sessions_controller.rb
+++ app/controllers/customers/sessions_controller.rb
@@ -2,6 +2,7 @@
 
 class Customers::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
+  before_action :customer_state, only: [:create]
   before_action :configure_permitted_parameters
 
   # GET /resource/sign_in
@@ -35,7 +36,18 @@ class Customers::SessionsController < Devise::SessionsController
   end
 
 
-  # protected
+  private
+
+  def customer_state
+    # 【処理内容1】 入力されたemailからアカウントを1件取得
+    @customer = Customer.find_by(email: params[:customer][:email])
+    # アカウントを取得できなかった場合、このメソッドを終了する
+    return if !@customer
+    # 【処理内容2】 取得したアカウントのパスワードと入力されたパスワードが一致してるかつ、is_deletedの値がtrueであるか
+    if @customer.valid_password?(params[:customer][:password]) && (@customer.is_deleted == true)
+        redirect_to customers_homes_welcome_path
+    end
+  end
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_in, keys: [:user_name, :introduction])
