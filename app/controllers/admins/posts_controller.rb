c1581098aa4f6d6dc36a01ccba507be87abb8517
ToriMun <132326048+torimun@users.noreply.github.com>
2023-09-29 18:23:21 +0000

    [Add]投稿承認詳細画面より調味・香辛料新規投稿が出来るよう作成

--- app/controllers/admins/posts_controller.rb
+++ app/controllers/admins/posts_controller.rb
@@ -6,17 +6,25 @@ class Admins::PostsController < ApplicationController
 
   def show
     @post_for_admin = PostForAdmin.find(params[:id])
+    @seasoning_spice = SeasoningSpice.new
   end
 
   def create
-    @post_for_admin = PostForAdmin.new(post_for_admin_params)
-    @post_for_admin.save
-    redirect_to admins_posts_path
+    @seasoning_spice = current_admin.seasoning_spices.build(seasoning_spice_params)
+    if @seasoning_spice.save
+      redirect_to admins_posts_path
+    else
+      render :new
+    end
   end
 
   private
 
   def post_for_admin_params
     params.require(:post_for_admin).permit(:image, :classification, :name, :detail, :usega)
+  end
+
+  def seasoning_spice_params
+    params.require(:seasoning_spice).permit(:image, :classification, :name, :detail, :usega)
   end
 end
